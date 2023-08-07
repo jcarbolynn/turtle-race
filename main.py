d719c479c09de77a0e57118c0649d57021827abb
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:08:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,38 @@
 from turtle import Turtle, Screen
 import random
 
-race_on = False
+is_race_on = False
 screen = Screen()
-screen.setup(width=500,height=400)
-
-user_bet = screen.textinput(title="make your bets now", prompt="which turtle will win the race? (color)")
-
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
+
+#Create 6 turtles
 for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(colors[turtle_index])
-    new_turtle.goto(x=-230, y=-70 + (turtle_index*30))
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
     all_turtles.append(new_turtle)
 
-# no way to change position based on for loop without creating another variable that increases with each color
-# for color in colors:
-#   name = str(color)
-#   name = Turtle(shape = "turtle")
-#   name.color(color)
-#   name.penup()
-#   name.goto(x=-240, y=-100)
-
 if user_bet:
-  race_on = True
+    is_race_on = True
 
-# so race only starts after user made a bet
-while race_on:
-  for turtle in all_turtles:
-    # race ends when turtle middle hits edge of screen, turtle object is 40x40 (250 - 40/2 = 230)
-    if turtle.xcor() > 230:
-      race_on = False
-      winning_color = turtle.pencolor()
-      if winning_color == user_bet:
-        print(f"You won! The {winning_color} turtle won")
-      else:
-        print(f"You lost! The {winning_color} turtle won")
-    
-    distance = random.randint(0,10) #(inclusive)
-    turtle.forward(distance)
+while is_race_on:
+    for turtle in all_turtles:
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-#keeps screen open until you close it by clicking on the screen
 screen.exitonclick()
